2b46b1f97407a25ef3af4208b49be34e6e07dc6f
Chris Smeele <cj.smeele@xs4all.nl>
2015-02-09 22:23:25 +0100

    Implemented memset, memcpy, strlen, atoi, abs

--- stage2/src/common.c
+++ stage2/src/common.c
@@ -1,21 +1,89 @@
 /**
- * @file
- * @brief
- * @author    Chris Smeele
- * @copyright Copyright (c) 2015, Chris Smeele
+ * \file
+ * \brief     Commonly used system, memory, string and math functions.
+ * \author    Chris Smeele
+ * \copyright Copyright (c) 2015, Chris Smeele. All rights reserved.
+ * \license   MIT. See LICENSE for the full license text.
  */
 #include "common.h"
 
 void halt() {
-	asm volatile(
+	asm volatile (
 		"hlt"
 	);
 }
 
 void hang() {
 	for (;;)
-		asm volatile(
+		asm volatile (
 			"cli\n"
 			"hlt\n"
 		);
+}
+
+void *memset(void *mem, uint8_t c, size_t length) {
+	if (length)
+		asm volatile (
+			"mloop: stosb\n"
+			"loop mloop\n"
+			:
+			: "a" (c),
+			  "c" (length),
+			  "D" (mem)
+			: "memory"
+		);
+
+	return mem;
+}
+
+void *memcpy(void *dest, const void *source, size_t length) {
+	for (size_t i=0; i<length; i++) {
+		((uint8_t*)dest)[i] = ((uint8_t*)source)[i];
+	}
+
+	return dest;
+}
+
+size_t strlen(const char *str) {
+	uint32_t i = 0;
+	while (*str++)
+		i++;
+	return i;
+}
+
+static uint32_t powU(uint32_t x, uint32_t y){
+	if(!y)
+		return 1;
+	uint32_t z = x;
+	for(uint32_t i=1; i<y; i++)
+		z *= x;
+	return z;
+}
+
+int atoi(const char *buf) {
+	bool negative = false;
+	int num = 0;
+
+	if (!buf[0])
+		return 0;
+
+	if (buf[0] == '-') {
+		negative = true;
+		buf++;
+	}
+
+	size_t len = strlen(buf);
+	for (ssize_t i=len-1; i>=0; i--) {
+		if (buf[i] < '0' || buf[i] > '9') {
+			return 0;
+		} else {
+			num += (buf[i] - '0') * powU(10, len-1-i);
+		}
+	}
+
+	return (negative ? -num : num);
+}
+
+int abs(int num) {
+	return num < 0 ? -num : num;
 }
--- stage2/src/common.h
+++ stage2/src/common.h
@@ -1,8 +1,9 @@
 /**
- * @file
- * @brief
- * @author    Chris Smeele
- * @copyright Copyright (c) 2015, Chris Smeele
+ * \file
+ * \brief     Commonly used system, memory, string and math functions.
+ * \author    Chris Smeele
+ * \copyright Copyright (c) 2015, Chris Smeele. All rights reserved.
+ * \license   MIT. See LICENSE for the full license text.
  */
 #ifndef _COMMON_H
 #define _COMMON_H
@@ -13,7 +14,63 @@
 
 #include "types.h"
 
+/**
+ * \brief Halt the CPU, wait for the next interrupt.
+ */
 void halt();
+
+/**
+ * \brief Hang forever
+ */
 void hang() __attribute__((noreturn));
+
+/**
+ * \brief Set length bytes starting at mem to c.
+ *
+ * \param mem
+ * \param c
+ * \param length
+ *
+ * \return a pointer to mem
+ */
+void *memset(void *mem, uint8_t c, size_t length);
+
+/**
+ * \brief Copy length bytes from source to dest.
+ *
+ * \param dest
+ * \param source
+ * \param length
+ *
+ * \return a pointer to dest
+ */
+void *memcpy(void *dest, const void *source, size_t length);
+
+/**
+ * \brief Returns the length of the input string.
+ *
+ * \param str a null-terminated string
+ *
+ * \return
+ */
+size_t strlen(const char *str);
+
+/**
+ * \brief Extract an integer from a string.
+ *
+ * \param buf a null-terminated string containing only a decimal numbers, and optonally a '-' sign
+ *
+ * \return
+ */
+int atoi(const char *buf);
+
+/**
+ * \brief Get the absolute value of an integer.
+ *
+ * \param num
+ *
+ * \return
+ */
+int abs(int num);
 
 #endif /* _COMMON_H */
