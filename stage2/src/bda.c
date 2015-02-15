13ff695a7c915c0278dd1d700b390fb22c2acb02
Chris Smeele <cj.smeele@xs4all.nl>
2015-02-15 22:32:50 +0100

    Added an incomplete BDA struct

---
+++ stage2/src/bda.c
@@ -0,0 +1,10 @@
+/**
+ * \file
+ * \brief     BIOS Data Area.
+ * \author    Chris Smeele
+ * \copyright Copyright (c) 2015, Chris Smeele. All rights reserved.
+ * \license   MIT. See LICENSE for the full license text.
+ */
+#include "bda.h"
+
+const BDA *const bda = (BDA*)0x400;
---
+++ stage2/src/bda.h
@@ -0,0 +1,25 @@
+/**
+ * \file
+ * \brief     BIOS Data Area.
+ * \author    Chris Smeele
+ * \copyright Copyright (c) 2015, Chris Smeele. All rights reserved.
+ * \license   MIT. See LICENSE for the full license text.
+ */
+#ifndef _BDA_H
+#define _BDA_H
+
+#include "common.h"
+
+/**
+ * \brief BIOS Data Area structure.
+ */
+typedef struct {
+
+	uint8_t _stuff[0x75]; // TODO.
+	uint8_t hdCount; ///< Amount of installed hard disks.
+
+} __attribute__((packed)) BDA;
+
+extern const BDA *const bda;
+
+#endif /* _BDA_H */
